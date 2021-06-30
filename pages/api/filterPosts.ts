cbf14830258b975287d9f625902b46b56ad17cc4
yu-yagishita <yu.yagishita1995@gmail.com>
2021-07-01 00:48:21 +0900

    feat: 型付けのエラーになってた

--- pages/api/filterPosts.ts
+++ pages/api/filterPosts.ts
@@ -5,6 +5,7 @@ import prisma from '../../lib/prisma'
 // GET /api/filterPosts?searchString=:searchString
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const { searchString } = req.query
+  if (typeof searchString === 'string') {
   const resultPosts = await prisma.post.findMany({
     where: {
       OR: [
@@ -18,4 +19,5 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
     },
   })
   res.json(resultPosts)
+  }
 }
